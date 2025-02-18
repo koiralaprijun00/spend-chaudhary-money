b516f31a47f1a52996037bab418eab84a86e2822
Prijun Koirala <prijun@kth.se>
2025-02-18 15:49:39 +0100

    Added Navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import Link from "next/link"
+import { FaTwitter } from "react-icons/fa" // Import the Twitter icon
+
+const Navbar = () => {
+  return (
+    <nav className="p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        {" "}{/* Added justify-between back */}
+        <Link href="/" className="flex items-center">
+          <Image src="/images/momo.png" alt="Piro Momo Logo" width={40} height={40} className="mr-2" />
+          <span className="text-gray-800 dark:text-white text-xl font-bold">Piro Momo</span>
+        </Link>
+        {/* Right side: Twitter Icon */}
+        <div className="flex items-center">
+          {" "}{/* Added a div for alignment */}
+          <Link href="https://x.com/kastopiromomo" target="_blank" rel="noopener noreferrer">
+            {" "}{/* Replace with your Twitter link */}
+            <FaTwitter className="text-gray-400 text-2xl hover:text-gray-800" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
