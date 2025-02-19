5f19b039a5d4dfd46f1eade55829dfb44b7e9b24
Prijun Koirala <prijun@kth.se>
2025-02-19 01:12:32 +0100

    Added Head from Next.JS - Better Integration with framework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import NavBar from "./components/NavBar";
+import Head from "next/head"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import NavBar from "./components/NavBar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  subsets: ["latin"]
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  subsets: ["latin"]
+})
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         {/* Google Adsense */}
         <meta name="google-adsense-account" content="ca-pub-4708248697764153" />
         {/* Open Graph Tags */}
@@ -30,16 +30,12 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://piromomo.com/path/to/binod-transparent.png" />
         {/* Adsense Script */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4708248697764153"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4708248697764153" crossOrigin="anonymous" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NavBar />
         {children}
       </body>
     </html>
-  );
+  )
 }
