7c265531faf740374c5d3dc63c0f22441c3ca0b6
Prijun Koirala <191650188+koiralaprijun00@users.noreply.github.com>
2025-04-02 16:23:18 +0200

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Mapbox CSS is already imported at build time, but if you need it dynamically: */}
         <link
