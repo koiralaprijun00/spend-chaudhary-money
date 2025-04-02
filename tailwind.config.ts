ebc80c4403bf4717aed0f491092e17856139fa4c
Prijun Koirala <191650188+koiralaprijun00@users.noreply.github.com>
2025-04-03 00:10:03 +0200

    tailwind update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,7 +37,6 @@ export default {
     '3xl': '1920px',
     '4xl': '2560px',
   },
-  darkMode: 'class', // Use 'class' for manual control
   plugins: [
     require('@tailwindcss/typography')
   ],
